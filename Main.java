675ae9831675b6760c5ab0694e77a4d3688d42b1
Zarif Ahmed <144474613+ZarifAhmed909@users.noreply.github.com>
2024-02-09 12:26:10 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Main{
+  public static void main(String[] args){
+    ArrayList<Integer> myArr = new ArrayList<>(){{
+      add(92);
+      add(95);
+      add(98);
+      add(80);
+      add(90);
+    }};
+
+    
+
+    
+  }
+
+
+}
+  
